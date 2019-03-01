b15c1c4778d97ae1856f0fb23d0181982f1ff10a
20084163KP <kevinp3099@gmail.com>
2019-03-01 14:26:52 +0000

    removing files from master to put on another branch

--- KevinPython/pi-print.py
+++
@@ -1,9 +0,0 @@
-Python 3.5.3 (default, Sep 27 2018, 17:25:39) 
-[GCC 6.3.0 20170516] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> pi = 3.14159;
->>> print("Pi =", pi)
-Pi = 3.14159
->>> print("or", 3.14, "for short")
-or 3.14 for short
->>> 
