4bcab49c35214ca36d63876e90894e4d0b4dfab7
20084163KP <kevinp3099@gmail.com>
2019-03-01 14:34:33 +0000

    uploading to the correct branch

---
+++ kevinPython/escapequotes.py
@@ -0,0 +1,13 @@
+Python 3.5.3 (default, Sep 27 2018, 17:25:39) 
+[GCC 6.3.0 20170516] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Did you know that 'word' is a word?")
+Did you know that 'word' is a word?
+>>> 
+print('Did you know that "word" is a word?')
+Did you know that "word" is a word?
+>>> print('Did you know that \'word\' is a word?')
+Did you know that 'word' is a word?
+>>> print("Did you know that \"word\" is a word?")
+Did you know that "word" is a word?
+>>> 
